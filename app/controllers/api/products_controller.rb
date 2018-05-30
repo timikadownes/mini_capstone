8312d5143147f158996908fffeb5e7ea856e2646
timika <timika.hopkins@gmail.com>
2018-05-29 20:16:28 -0400

    est route, controller action and view file

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,7 @@
 class Api::ProductsController < ApplicationController
 
-  
+  def products_action
+    render "product_view.json.jbuilder"
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
 
-get "/=>"
+  namespace :api do 
+    get "/products_url" => "products#products_action"
+  end
 end
-
